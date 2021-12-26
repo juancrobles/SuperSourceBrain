57a31e53e9fc895af6ba1b67b9df00c88b6c699a
Juan C <project.ufa@gmail.com>
2021-12-26 07:41:05 -0600

    added Process & Reset input tests added test scenario when we have one panelist in the queue added definition of inputProcess & inputReset added code to satisfy the new tests

--- js/SuperSourceBrain.js
+++ js/SuperSourceBrain.js
@@ -8,7 +8,10 @@ function SuperSourceBrain() {}
  * Define all internal variables to a known value
  */
 SuperSourceBrain.prototype.baString = "";
+SuperSourceBrain.prototype.odlBAString = "";
 SuperSourceBrain.prototype.superSourceName = "NOSOURCE";
+SuperSourceBrain.prototype.baArray = [];
+SuperSourceBrain.prototype.baCount = 0;
 
 
 /**
@@ -16,8 +19,29 @@ SuperSourceBrain.prototype.superSourceName = "NOSOURCE";
  * @param {*} ba_string 
  */
 SuperSourceBrain.prototype.inputBAString = function(ba_string) {
-    this.baString = ba_string;
+    if(this.baString !== ba_string) {
+        this.odlBAString = this.baString;
+        this.baString = ba_string;
+    }
+    
+    this.baArray = this.baString.match(/[^\r\n]+/g);
+    this.baCount = this.baArray.length;
 };
+
+/**
+ * Define the input process
+ */
+SuperSourceBrain.prototype.inputProcess = function() {
+    if(this.baCount == 1)
+        this.superSourceName = "1";
+}
+
+/**
+ * Definte the input reset
+ */
+SuperSourceBrain.prototype.inputReset = function() {
+
+}
 
 /**
  * Define the output SuperSourceName
--- spec/SuperSourceBrainSpec.js
+++ spec/SuperSourceBrainSpec.js
@@ -12,6 +12,14 @@ describe("SuperSourceBrain", function(){
         it("inputBAString", function() {
             expect(typeof (superSourceBrain.inputBAString)).toBe("function")
         });
+
+        it("inputProcess", function() {
+            expect(typeof (superSourceBrain.inputProcess)).toBe("function")
+        });
+
+        it("inputReset", function() {
+            expect(typeof (superSourceBrain.inputReset)).toBe("function")
+        });
     });
 
     describe("should have the output", function() {
@@ -23,7 +31,23 @@ describe("SuperSourceBrain", function(){
     describe("with no panelists id on inputBAString", function () {
         it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
             superSourceBrain.inputBAString("");
+            
             expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+        });
+    });
+
+    describe('with one panelist id on inputBAString', () => {
+        it('should return "NOSOURCE" on the outputSuperSourceName', () => {
+            superSourceBrain.inputBAString("1234");
+
+            expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+        });
+
+        it('and triggering the input Process should return "1" on the outputSuperSourceName', () => {
+            superSourceBrain.inputBAString("1234");
+            superSourceBrain.inputProcess();
+
+            expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
         });
     });
 });
