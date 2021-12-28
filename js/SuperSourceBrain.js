05fa42f5db70fdccfb909a0387107490695b0810
Juan C <project.ufa@gmail.com>
2021-12-28 04:42:08 -0600

    added tests for inputs-outputs added test for validating inputs added test for run through the question with one panelist added test for run through the question with two panelist added code to satisfy the tests updated isadora sample code added API simulations on isadora

--- js/SuperSourceBrain.js
+++ js/SuperSourceBrain.js
@@ -2,66 +2,176 @@
 /**
  * Define the UserActor container 
  */
-function SuperSourceBrain() {}
+ function SuperSourceBrain() {}
 
-/**
- * Define all internal variables to a known value
- */
-SuperSourceBrain.prototype.baString = "";
-SuperSourceBrain.prototype.odlBAString = "";
-SuperSourceBrain.prototype.superSourceName = "NOSOURCE";
-SuperSourceBrain.prototype.baArray = [];
-SuperSourceBrain.prototype.baCount = 0;
-SuperSourceBrain.prototype.processCount = 0;
-
-
-/**
- * Define the input BAString
- * @param {*} ba_string 
- */
-SuperSourceBrain.prototype.inputBAString = function(ba_string) {
-    if(this.baString !== ba_string) {
-        this.odlBAString = this.baString;
-        this.baString = ba_string;
-    }
-    
-    this.baArray = this.baString.match(/[^\r\n]+/g);
-
-    if(this.baArray !== null)
-        this.baCount = this.baArray.length;
-};
-
-/**
- * Define the input process
- */
-SuperSourceBrain.prototype.inputProcess = function() {
-    if(this.baCount == 1)
-        this.superSourceName = "1";
-
-    if(this.processCount >= this.baCount) {
-        this.superSourceName = "NOSOURCE";
-        this.baArray = [];
-        this.baCount = 0;
-    }
-
-    this.processCount++;
-}
-
-/**
- * Definte the input reset
- */
-SuperSourceBrain.prototype.inputReset = function() {
-
-}
-
-/**
- * Define the output SuperSourceName
- * @returns the active super source name
- */
-SuperSourceBrain.prototype.outputSuperSourceName = function() {
-    return this.superSourceName;
-}
-
+ /**
+  * Define all internal variables to a known value
+  */
+ SuperSourceBrain.prototype.apiData = null;
+ SuperSourceBrain.prototype.apiSuccess = false;
+ SuperSourceBrain.prototype.superSourceName = "NOSOURCE";
+ SuperSourceBrain.prototype.superSourceUpdated = false;
+ // SuperSourceBrain.prototype.visiblePanelists = 0;
+ // SuperSourceBrain.prototype.invisiblePanelists = 0;
+ // SuperSourceBrain.prototype.currentPanelist = -1;
+ SuperSourceBrain.prototype.maxBoxes = 3;
+ 
+ 
+ /**
+  * Define the input HandsAPI
+  * @param {*} data 
+  */
+ SuperSourceBrain.prototype.inputHandsAPI = function(data) {
+     if( data === null || data === 'undefined' || data.length === 0 )
+         return;
+ 
+     this.apiData = data;
+ };
+ 
+ /**
+  * Define the input success API
+  */
+ SuperSourceBrain.prototype.inputSuccessAPI = function(result) {
+     this.apiSuccess = result == 1 ? true : false;
+ 
+     if(
+         this.apiSuccess == true && this.apiData !== null && 
+         this.apiData !== "" && this.apiData !== 'undefined') {
+         this.updateStatus();
+     }
+ }
+ 
+ /**
+  * Define the output SuperSourceName
+  * @returns the active super source name
+  */
+ SuperSourceBrain.prototype.outputSuperSourceName = function() {
+     return this.superSourceName;
+ }
+ 
+ SuperSourceBrain.prototype.outputSuperSourceUpdated = function() {
+     return this.superSourceUpdated;
+ }
+ 
+ SuperSourceBrain.prototype.updateStatus = function() {
+     // console.log("updateStatus");
+ 
+     // parsing received data
+     var panelistCounts = this.parseData();
+ 
+     var totalPanelists = panelistCounts[1] + panelistCounts[2];
+     var startSubstring = panelistCounts[2];
+     // console.log("total panelists: ", totalPanelists, "start substring: ", startSubstring);
+ 
+     // build array of boxes
+     var boxCount = 1;
+     var boxesArray = [];
+     for( i = 0; i < totalPanelists; i++) {
+         if(boxCount > this.maxBoxes)
+             boxCount = 1;
+         
+         boxesArray.push(boxCount.toString(10));
+ 
+         boxCount++;
+     }
+ 
+     // console.log("boxesArray:", boxesArray.join(''));
+     var boxesString = boxesArray.join('');
+ 
+     if(totalPanelists !== 0 ) {
+ 
+         var substring = "NOSOURCE";
+         if((startSubstring + this.maxBoxes) > totalPanelists) {
+             substring = boxesString.substring(startSubstring);
+         } else {
+             substring = boxesString.substring(startSubstring, startSubstring + this.maxBoxes);
+         }
+         
+         // console.log("substring: ", substring);
+         
+         if(substring === "")
+             substring = 'NOSOURCE';
+ 
+         this.superSourceName = substring;
+         this.superSourceUpdated = true;
+     } else {
+         this.superSourceName = 'NOSOURCE';
+         this.superSourceUpdated = true;
+     }
+     // updata output data
+ /*    switch (panelistCount) {
+         case 1:
+             this.superSourceName = '1';
+             this.superSourceUpdated = true;
+         break;
+     
+         case 2:
+             this.superSourceName = '12';
+             this.superSourceUpdated = true;
+         break;
+     
+         default:*/
+ /*            break;
+     }*/
+ }
+ 
+ SuperSourceBrain.prototype.parseData = function() {
+     // split data into array
+     // console.log("content of data", this.apiData);
+     var dataArray = this.apiData.match(/[^\r\n]+/g);
+ 
+     var visiblePanelists = 0;
+     var invisiblePanelists = 0;
+     var currentPanelist = -1;
+ 
+     for( i = 0; i < dataArray.length; i++) {
+         // test if we have multiple values
+         if(dataArray[i].includes(',')) {
+             // console.log("this line contains multiple values");
+             // processing multiple panelists
+             var panelistsArray = dataArray[i].split(',');
+             // console.log("panelists count: ", panelistsArray.length);
+             for( j = 0; j < panelistsArray.length; j++) {
+                 // console.log("state:", i, "panelist No.", j, "panelist: ", panelistsArray);
+                 if(panelistsArray === 'NONE' || panelistsArray === 'undefined') {
+                     // console.log("invalid id");
+     
+                 } else {
+                     // console.log("valid id");
+                     // test if we are not the previous panelists increase panelist count
+                     if(i < 2)
+                         visiblePanelists++;
+                     else
+                         invisiblePanelists++;
+ 
+                     /*if(i == 1)
+                         currentPanelist = i;*/
+                 }
+             }
+         } else {
+             // processing one panelist
+             // console.log("this line only contains one value", dataArray[i]);
+ 
+             // test for invalid id
+             if(dataArray[i] === 'NONE' || dataArray[i] === 'undefined') {
+                 // console.log("invalid id");
+ 
+             } else {
+                 // console.log("valid id");
+                 // this is a special case when only have one panelist
+                 if(i < 2)
+                     visiblePanelists++;
+                 else
+                     invisiblePanelists++;
+                 
+                 if(i == 1)
+                     currentPanelist = i;
+             }
+         }
+     }
+     return [currentPanelist, visiblePanelists, invisiblePanelists];
+ }
+ 
 
 // this line is necesary for running the tests and 
 // should not be copied to the isadora javascript actor
--- spec/SuperSourceBrainSpec.js
+++ spec/SuperSourceBrainSpec.js
@@ -9,16 +9,12 @@ describe("SuperSourceBrain", function(){
       });
 
     describe("should have the input", function() {
-        it("inputBAString", function() {
-            expect(typeof (superSourceBrain.inputBAString)).toBe("function")
+        it("inputHandsAPI", function() {
+            expect(typeof (superSourceBrain.inputHandsAPI)).toBe("function")
         });
 
-        it("inputProcess", function() {
-            expect(typeof (superSourceBrain.inputProcess)).toBe("function")
-        });
-
-        it("inputReset", function() {
-            expect(typeof (superSourceBrain.inputReset)).toBe("function")
+        it("inputSuccessAPI", function() {
+            expect(typeof (superSourceBrain.inputSuccessAPI)).toBe("function")
         });
     });
 
@@ -26,36 +22,311 @@ describe("SuperSourceBrain", function(){
         it("outputSuperSourceName", function() {
             expect(typeof (superSourceBrain.outputSuperSourceName)).toBe("function")
         });
-    });
 
-    describe("with no panelists id on inputBAString", function () {
-        it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
-            superSourceBrain.inputBAString("");
-
-            expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+        it("outputSuperSourceUpdated", function() {
+            expect(typeof (superSourceBrain.outputSuperSourceUpdated)).toBe("function")
         });
     });
 
-    describe('with one panelist id on inputBAString', () => {
-        it('should return "NOSOURCE" on the outputSuperSourceName', () => {
-            superSourceBrain.inputBAString("1234");
-
-            expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+    describe("with invalid data", function () {
+        describe("and success on the API request", function() {
+            it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("undefined\nundefined\nundefined");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
         });
 
-        it('and triggering the input Process should return "1" on the outputSuperSourceName', () => {
-            superSourceBrain.inputBAString("1234");
-            superSourceBrain.inputProcess();
-
-            expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
-        });
-
-        it('and triggering for second time the input Process should return "NOSOURCE" on the outputSuperSourceName', () => {
-            superSourceBrain.inputBAString("1234");
-            superSourceBrain.inputProcess();
-            superSourceBrain.inputProcess();
-
-            expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+        describe("and failure on the API request", function() {
+            it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("undefined\nundefined\nundefined");
+                superSourceBrain.inputSuccessAPI(0);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
         });
     });
+
+    describe("with no panelists on the question", function () {
+        describe("and success on the API request", function() {
+            it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+
+        describe("and failure on the API request", function() {
+            it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(0);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+    });
+
+    describe('with one panelist as upcoming answerer', () => {
+        describe("and success on the API request", function() {
+            it("should return '1' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("1254\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual('1');
+            });
+        });
+
+        describe("and failure on the API request", function() {
+            it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("1254\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(0);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+
+    });
+
+    describe('with one panelist as current answerer', () => {
+        describe("and success on the API request", function() {
+            it("should return '1' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("NONE\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual('1');
+            });
+        });
+
+        describe("and failure on the API request", function() {
+            it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("NONE\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(0);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+
+    });
+
+    describe('with one panelist as previous answerer', () => {
+        describe("and success on the API request", function() {
+            it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\n1254");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual('NOSOURCE');
+            });
+        });
+
+        describe("and failure on the API request", function() {
+            it("should return 'NOSOURCE' on the outputSuperSourceName", function() {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\n1254");
+                superSourceBrain.inputSuccessAPI(0);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+
+    });
+
+    describe('with one panelist cycle through the question', function() {
+        describe('added as upcoming asnwerer', function() {
+            it('should return "1" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+            });
+        });
+        describe('changed to current asnwerer', function() {
+            it('should return "1" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+
+                superSourceBrain.inputHandsAPI("NONE\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+            });
+        });
+        describe('changed to previous asnwerer', function() {
+            it('should return "1" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+
+                superSourceBrain.inputHandsAPI("NONE\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+
+                superSourceBrain.inputHandsAPI("NONE\nNONE\n1254");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+        describe('flushed out as asnwerer', function() {
+            it('should return "NOSOURCE" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+
+                superSourceBrain.inputHandsAPI("NONE\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+
+                superSourceBrain.inputHandsAPI("NONE\nNONE\n1254");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+    });
+
+    describe('with two panelist cycle through the question', function() {
+        describe('added panelists as upcoming asnwerers', function() {
+            it('should return "12" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254,1125\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("12");
+            });
+        });
+        describe('changed first panelist to current asnwerer', function() {
+            it('should return "12" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254,1125\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("12");
+
+                superSourceBrain.inputHandsAPI("1125\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("12");
+            });
+        });
+        describe('changed second panelist to current asnwerer', function() {
+            it('should return "2" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254,1125\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("12");
+
+                superSourceBrain.inputHandsAPI("1125\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("12");
+
+                superSourceBrain.inputHandsAPI("NONE\n1125\n1254");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("2");
+            });
+        });
+        describe('changed second panelist to previous asnwerer', function() {
+            it('should return "NOSOURCE" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254,1125\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("12");
+
+                superSourceBrain.inputHandsAPI("1125\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("12");
+
+                superSourceBrain.inputHandsAPI("NONE\n1125\n1254");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("2");
+
+                superSourceBrain.inputHandsAPI("NONE\nNONE\n1254,1125");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+
+        xdescribe('flushed out as asnwerer', function() {
+            it('should return "NOSOURCE" on the ourputSourceName', () => {
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+
+                superSourceBrain.inputHandsAPI("1254\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+
+                superSourceBrain.inputHandsAPI("NONE\n1254\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+
+                superSourceBrain.inputHandsAPI("NONE\nNONE\n1254");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("1");
+
+                superSourceBrain.inputHandsAPI("NONE\nNONE\nNONE");
+                superSourceBrain.inputSuccessAPI(1);
+    
+                expect(superSourceBrain.outputSuperSourceName()).toEqual("NOSOURCE");
+            });
+        });
+    })
 });
